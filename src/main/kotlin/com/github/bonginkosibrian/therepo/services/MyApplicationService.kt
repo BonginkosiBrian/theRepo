0fe83ffbf6c8e05c70488616221fd0d6c98b2570
GitHub Action <action@github.com>
2021-07-12 18:20:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/bonginkosibrian/therepo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bonginkosibrian/therepo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bonginkosibrian.therepo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bonginkosibrian.therepo.MyBundle
 
 class MyApplicationService {
 
